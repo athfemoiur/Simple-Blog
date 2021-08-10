190c63ba0af0c9e216b7007f801abb08eaaba726
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-10 19:57:46 +0430

    start project and add apps

---
+++ user/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    date_of_birth = models.DateField(blank=True, null=True)
+    avatar = models.ImageField(null=True, blank=True, upload_to='user/avatars/')
+    score = models.PositiveSmallIntegerField(default=0)
+
+    def get_date_of_birth(self):
+        return self.date_of_birth
+
