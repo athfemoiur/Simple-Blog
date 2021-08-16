280ee73371608a0906b20bcb8a6c7a27d9f61156
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-16 13:01:04 +0430

    fix category pages and change search bar ph

--- category/views.py
+++ category/views.py
@@ -8,5 +8,10 @@ class CategoryPostsView(ListView):
     template_name = 'category/category_posts.html'
     context_object_name = 'posts'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=None, **kwargs)
+        context['category'] = Category.objects.get(id=self.kwargs['pk'])
+        return context
+
     def get_queryset(self):
-        return Category.objects.get(id=self.kwargs['pk']).posts.all()
+        return Category.objects.get(id=self.kwargs['pk']).posts.filter(status=1)
--- user/forms.py
+++ user/forms.py
@@ -18,15 +18,16 @@ class DateInput(forms.DateInput):
 
 
 class CustomUserCreationForm(UserCreationForm):
+    avatar = forms.FileField(required=False, widget=forms.FileInput(attrs={'class': 'form-control'}))
+
     class Meta(UserCreationForm.Meta):
-        avatar = forms.FileField()
         model = User
         fields = ('username', 'password1', 'password2', 'email', 'first_name', 'last_name', 'avatar', 'date_of_birth')
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control'}),
-            'password1': forms.TextInput(attrs={'class': 'form-control'}),
-            'password2': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.TextInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'first_name': forms.TextInput(attrs={'class': 'form-control'}),
             'last_name': forms.TextInput(attrs={'class': 'form-control'}),
             'date_of_birth': DateInput(attrs={'class': 'form-control'}),
