4a4d22cd09e2d9e7a10e0df7ded4506597e4ae1b
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-16 20:58:47 +0430

    add custom templates for reset password process

--- user/urls.py
+++ user/urls.py
@@ -11,8 +11,15 @@ urlpatterns = [
     path('<int:pk>/profile-edit', UpdateProfileView.as_view(), name='profile-edit'),
     path('<int:pk>/posts/', UserPostListView.as_view(), name='user-post-list'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('reset-password', PasswordResetView.as_view(), name='reset_password'),
-    path('reset-password-sent', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset-password-change', PasswordResetCompleteView.as_view(), name='password_reset_complete')
+    path('reset-password', PasswordResetView.as_view(template_name='user/reset-password/reset_password.html'),
+         name='reset_password'),
+    path('reset-password-sent',
+         PasswordResetDoneView.as_view(template_name='user/reset-password/reset_password_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>',
+         PasswordResetConfirmView.as_view(template_name='user/reset-password/reset_password_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset-password-change',
+         PasswordResetCompleteView.as_view(template_name='user/reset-password/reset_password_complete.html'),
+         name='password_reset_complete')
 ]
